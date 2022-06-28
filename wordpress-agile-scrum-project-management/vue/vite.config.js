9024d9a5b3aa59e79dbaa475c108f25ac612fbbd
Kiril Kirkov <kiro_tyson@abv.bg>
2022-06-28 09:59:38 +0300

    t

--- wordpress-agile-scrum-project-management/vue/vite.config.js
+++ wordpress-agile-scrum-project-management/vue/vite.config.js
@@ -3,8 +3,11 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}`,
   plugins: [vue()],
   resolve: {
     alias: {
