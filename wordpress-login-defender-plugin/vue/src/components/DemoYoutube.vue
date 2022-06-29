b28fcbce374fe3ff94bf5c4685206e351252d260
Kiril Kirkov <kiro_tyson@abv.bg>
2022-06-29 22:44:03 +0300

    add demo for login defender

--- wordpress-login-defender-plugin/vue/src/components/DemoYoutube.vue
+++ wordpress-login-defender-plugin/vue/src/components/DemoYoutube.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe width="100%" height="315" src="https://www.youtube.com/embed/jNQXAC9IVRw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/MijSsDagGCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </template>
 <style scoped>
 iframe {
